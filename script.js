b8facf2e539087b8e6960a9c2569904ef8ab174c
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2024-07-20 16:54:26 +0000

    Begin script parsing

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 
 // Where the scripting will go
+let scriptContent = "";
+
+function runScript () {
+  let scriptParsed = scriptContent.split("\n");
+  for (let i = 0; i < scriptParsed.length; i++) {
+    
+  }
+}
+
+// Random events
+
+function decideEvent () {
+  let randomNum = Math.floor(Math.random() * 5);
+}
